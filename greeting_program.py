ea5712c1f6284ddd5a77e5cae253361d428a6981
TahaAnsari5359 <tahairfanansari@gmail.com>
2025-06-20 18:50:30 +0530

    Add files via upload

---
+++ greeting_program.py
@@ -0,0 +1,6 @@
+Enter_1st_name = input("Enter Your First Name: ")
+Enter_2nd_name = input("Enter Your Last Name: ")
+Full_name = Enter_1st_name +" "+ Enter_2nd_name
+
+
+print("Hello", Full_name+"!", "Welcome to the python program.")
---
+++ simple calculator.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter Number 1: "))
+num2 = int(input("Enter Number 2: "))
+
+add = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+divide = num1/num2
+
+print(f" Sum - {add} \n Subtraction - {sub} \n Multi - {multi} \n Divide - {divide}")
